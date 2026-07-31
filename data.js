eed7940fd23465745d2d240595629c730affb04a
jouinintei <sssssssshi25@gmail.com>
2026-08-01 00:30:46 +0900

    削除: 134057659912721471

--- data.js
+++ data.js
@@ -95,5 +95,4 @@ window.NURIE_LIST = [
   { kind: "カレンダー", year: 2026, month: 9, season: "通年", title: "9月カレンダー", file: "calendar_2026_09.png" },
   { kind: "カレンダー", year: 2026, month: 9, season: "通年", title: "9月カレンダー(貼り付け用)", file: "calendar_2026_09_blank.png" },
   { kind: "カレンダー", year: 2026, month: 10, season: "通年", title: "10月カレンダー(貼り付け用)", file: "calendar_2026_10_blank.png" },
-  { kind: "カレンダー", year: 2026, season: "通年", title: "134057659912721471", file: "134057659912721471.jpg" },
 ];
