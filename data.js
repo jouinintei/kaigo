3c76f73f8252c0292d6dcb1e49d200e098822e62
jouinintei <sssssssshi25@gmail.com>
2026-08-14 21:30:57 +0900

    投稿: 1枚追加

--- data.js
+++ data.js
@@ -540,4 +540,5 @@ window.NURIE_LIST = [
   { season: "秋", genre: "食べ物", title: "和菓子", diff: "初級", file: "up_1786698965869_1.png", added: "2026-08" },
   { season: "夏", genre: "風物詩", title: "花火", diff: "上級", file: "up_1786699872178_2.png", added: "2026-08" },
   { season: "夏", genre: "風物詩", title: "送り火", diff: "上級", file: "up_1786709449497_1.png", added: "2026-08" },
+  { season: "夏", genre: "風物詩", title: "送り火", diff: "初級", file: "up_1786710655144_2.png", added: "2026-08" },
 ];
