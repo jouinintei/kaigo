c5a8e657d2ae75eb4a344e33b6fd065b724687fd
jouinintei <sssssssshi25@gmail.com>
2026-08-14 18:31:14 +0900

    投稿: 1枚追加

--- data.js
+++ data.js
@@ -538,4 +538,5 @@ window.NURIE_LIST = [
   { season: "秋", genre: "食べ物", title: "さんま定食", diff: "中級", file: "up_1786697869414_1.png", added: "2026-08" },
   { kind: "カレンダー", year: 2026, month: 9, season: "通年", title: "9月カレンダー7", file: "up_1786698468255_2.png", added: "2026-08" },
   { season: "秋", genre: "食べ物", title: "和菓子", diff: "初級", file: "up_1786698965869_1.png", added: "2026-08" },
+  { season: "夏", genre: "風物詩", title: "花火", diff: "上級", file: "up_1786699872178_2.png", added: "2026-08" },
 ];
