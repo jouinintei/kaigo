d94499ad8a56d4c892be240d6d89b1289626df52
jouinintei <sssssssshi25@gmail.com>
2026-08-22 12:46:02 +0900

    投稿: 1枚追加

--- data.js
+++ data.js
@@ -552,4 +552,5 @@ window.NURIE_LIST = [
   { season: "通年", genre: "風景", title: "キャンプ", diff: "上級", file: "up_1787323089737_9.png", added: "2026-08" },
   { season: "通年", genre: "風景", title: "美容室", diff: "中級", file: "up_1787370039418_10.png", added: "2026-08" },
   { season: "通年", genre: "風景", title: "昭和の美容室", diff: "中級", file: "up_1787370204076_11.png", added: "2026-08" },
+  { season: "通年", genre: "その他", title: "昭和のファッション", diff: "中級", file: "up_1787370358584_12.png", added: "2026-08" },
 ];
